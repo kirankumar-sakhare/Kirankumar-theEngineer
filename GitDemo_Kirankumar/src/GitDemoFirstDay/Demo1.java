031e3db25c46204977e4d961e9780e7f4c37b737
kirankumar-sakhare <115784816+kirankumar-sakhare@users.noreply.github.com>
2022-10-15 13:15:32 +0530

    Update Demo1.java

--- GitDemo_Kirankumar/src/GitDemoFirstDay/Demo1.java
+++ GitDemo_Kirankumar/src/GitDemoFirstDay/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1
 	public static void main(String[] args) 
 	{
 		System.out.println("It's me kirankumar GitHub");
+		System.out.println("It's me kirankumar GitHub Alpha ");
 		
 	}
 
